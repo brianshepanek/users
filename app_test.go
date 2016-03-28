15212f5821de72ed47f5e3ef661e4e4aba41df38
Brian Shepanek <brian@handprintdigital.com>
2016-03-28 15:17:18 -0400

    Add New Dummy Test

--- app_test.go
+++ app_test.go
@@ -10,3 +10,9 @@ func TestTruth(t *testing.T) {
         t.Error("everything I know is wrong")
     }
 }
+
+func TestTruth2(t *testing.T) {
+    if true != true {
+        t.Error("everything I know is wrong")
+    }
+}
