48406a7c7b289022d4a8c070c205acdbc6d1337e
Brian Shepanek <brian@handprintdigital.com>
2016-03-28 15:35:11 -0400

    Add Test 3

--- app_test.go
+++ app_test.go
@@ -16,3 +16,9 @@ func TestTruth2(t *testing.T) {
         t.Error("everything I know is wrong")
     }
 }
+
+func TestTruth3(t *testing.T) {
+    if true != true {
+        t.Error("everything I know is wrong")
+    }
+}
